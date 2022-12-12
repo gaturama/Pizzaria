2979aa7cd5a4016143596884d0ab3b45ba5045ab
Gabito <101840148+gaturama@users.noreply.github.com>
2022-12-12 11:25:13 -0300

    Update Pizza.java

--- Pizza.java
+++ Pizza.java
@@ -2,8 +2,8 @@ import java.util.ArrayList;
 
 public class Pizza {
 
-    public int id;
-    public String tamanho;
+    private int id;
+    private String tamanho;
 
     public static ArrayList<Pizza> pizzas = new ArrayList<Pizza>();
 
@@ -16,5 +16,31 @@ public class Pizza {
 
         pizzas.add(this);
     }
+    
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public String getTamanho(){
+        return tamanho;
+    }
+    public void setTamanho(String tamanho){
+        this.tamanho = tamanho;
+    }
+    public static Pizza verificarId(int id) throws Exception{
+        for(Pizza pizza : pizzas){
+            if(pizza.id == id){
+                return pizza;
+            }
+        }
+        throw new Exception("Produto não existe");
+    }
+    
+    @Override 
+    public String toString(){
+        return "ID: " + id + "\n"
+            + "Tamanho: " + tamanho + "\n";
  
 }   
